9ec7d02b0fd27adc73fc44dc57a027696193403c
Minaiev Kiril <kiril777kiril@gmail.com>
2022-05-08 23:25:47 +0300

    fixs

---
+++ js/task-10.js
@@ -0,0 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const input = document.querySelector("input")
+const boxes = document.querySelector("#boxes")
+const create = document.querySelector("[data-create]")
+const destroy = document.querySelector("[data-destroy]")
+
+const box = document.createElement("div")
+
+const inputValue = () => Number(input.value);
+
+
+function 
+
+create.addEventListener("mouseup", inputValue)
+
+
+
+console.log(box)
