e556f1cfa3cf0a0051bd8000f20120310b78e905
Minaiev Kiril <kiril777kiril@gmail.com>
2022-05-08 23:11:38 +0300

    9 task done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -2,3 +2,12 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const body = document.body;
+const button = document.querySelector(".change-color")
+const colorName = document.querySelector(".color")
+
+button.addEventListener("mouseup", event => {
+  body.style.backgroundColor = getRandomHexColor()
+  colorName.innerHTML = `${getRandomHexColor()}`
+});
+
