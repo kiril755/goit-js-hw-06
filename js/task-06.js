c66318d96990c9e803ac599207d124701643c1b0
Minaiev Kiril <kiril777kiril@gmail.com>
2022-05-14 17:42:30 +0300

    fixes

--- js/task-06.js
+++ js/task-06.js
@@ -12,7 +12,7 @@ const validationInput = document.querySelector("#validation-input")
 
 const checkLenght = (event) => {
     if (event.currentTarget.value.length != "") {
-        event.currentTarget.value.length >= validationInput.getAttribute("data-length") ?
+        event.currentTarget.value.length == validationInput.getAttribute("data-length") ?
         validationInput.classList.add("valid") : validationInput.classList.add("invalid")
     } else validationInput.classList.remove("valid" && "invalid")
 }
